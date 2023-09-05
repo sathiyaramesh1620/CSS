04dcd4b0fed2b2c8ff526b42edddc04662112d8a
sathiyaramesh <sathiyaramesh1620@gmail.com>
2023-09-05 16:48:23 +0530

    ok

---
+++ CSS Generator/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const  allBorder=document.getElementById('all_value');
+var all_radius=10;
+
+    function allBorderUpdate(){
+        // console.log('hi')
+        all_value.innerTextall+"px";
+
+    }
+    allBorder.addEventListener("mouseover",allBorderUpdate)
+    allBorder.addEventListener("change",allBorderUpdate)
+
+})
